dd0d628fce5483f0d7f8396efb853a38100f8dee
emigrek <jarasz.karol@gmail.com>
2023-07-14 12:59:46 +0200

    Fix pc/mobile Cell width sizing, remove blue touch highlight

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,8 +3,8 @@ import { motion as m } from 'framer-motion';
 
 const Loader: FC = () => {
   return (
-    <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="absolute inset-0 z-40 flex items-center justify-center w-full h-full overflow-hidden rounded-lg backdrop-blur bg-neutral-900/80">
-      <m.div initial={{ scale: 0, rotate: 180 }} animate={{ scale: 1, opacity: 0.5, rotate: 0 }} exit={{ scale: 0, opacity: 0 }} className='relative w-20 h-20 opacity-50 md:w-28 md:h-28 animate-pulse'>
+    <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="absolute inset-0 z-40 flex items-center justify-center w-full h-full overflow-hidden rounded-lg backdrop-blur bg-neutral-900/50">
+      <m.div initial={{ scale: 0, rotate: 180 }} animate={{ scale: 1, opacity: 1, rotate: 0 }} exit={{ scale: 0, opacity: 0 }} className='relative w-20 h-20 md:w-28 md:h-28 animate-pulse'>
         <img src="/logo_firlej.png" alt="Firlej Logo" className="w-full h-full" />
       </m.div>
     </m.div>
--- src/components/ui/Grid/Cell.tsx
+++ src/components/ui/Grid/Cell.tsx
@@ -5,7 +5,7 @@ type CellProps = HTMLAttributes<HTMLDivElement>;
 
 const Cell: FC<CellProps> = ({ className, ...props }) => {
     return (
-        <div className={cn("w-full h-[calc((100vh-theme(spacing.14)-3*theme(spacing.1))/4)] md:w-[calc((100vw-theme(spacing.1))/2)] md:h-[calc((100vh-theme(spacing.14)-theme(spacing.1))/2)] aspect-video", className)} {...props}/>
+        <div className={cn("w-auto h-[calc((100vh-theme(spacing.14)-3*theme(spacing.1))/4)] md:h-[calc((100vh-theme(spacing.14)-theme(spacing.1))/2)]", className)} {...props}/>
     )
 }
 
