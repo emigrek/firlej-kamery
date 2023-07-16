3ff18c5821a308a3c2cdfd1b728b7b5138bfba69
emigrek <jarasz.karol@gmail.com>
2023-07-17 01:57:55 +0200

    Fix wrong tailwind color

--- src/components/modals/CameraModal.tsx
+++ src/components/modals/CameraModal.tsx
@@ -5,7 +5,6 @@ import useMapStore from '@/stores/mapStore';
 import { customStyles } from '@/components/modals/shared';
 
 import Camera from '@/components/Camera';
-import { TbDeviceCctv } from 'react-icons/tb';
 
 const CameraModal: FC = () => {
     const { map } = useMapStore();
@@ -21,7 +20,7 @@ const CameraModal: FC = () => {
     return (
         <Modal closeTimeoutMS={200} isOpen={isOpen} style={customStyles} onRequestClose={handleCameraModalClose}>
             <div className="flex flex-col gap-5">
-                <h1 className="flex items-center justify-center gap-2 text-xl font-bold text-center md:text-3xl text-neutral-100">
+                <h1 className="flex items-center justify-center gap-2 text-xl font-bold text-center md:text-3xl text-neutral-200">
                     {camera?.name}
                 </h1>
                 <div className='relative px-2 md:w-auto w-screen md:h-[60vh] aspect-video'>
